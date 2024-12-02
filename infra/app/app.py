b873a6a7d8a2578073f4783998a144b0b2cda0d8
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-12-02 06:45:45 +0300

    [2/N] fix: CORS error

--- infra/app/app.py
+++ infra/app/app.py
@@ -10,7 +10,7 @@ import joblib
 from model.get_geolocation import get_geolocation
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 logging.basicConfig(level=logging.INFO)
 recent_requests = deque(maxlen=10)
 UNKNOWN = 'UNKNOWN'
@@ -32,6 +32,15 @@ def safe_transform(encoder, value):
         return encoder.transform([value])[0]
     else:
         return encoder.transform([UNKNOWN])[0]
+
+@app.route('/analyze', methods=['OPTIONS'])
+def handle_options():
+    response = Flask.Response()
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization'
+    return response
+
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
--- webapp/lib/const.ts
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = "http://api.thesmolentsev.ru/analyze";
-export const GITHUB_URL = "https://github.com/s1ckoleg/ml_curs";
-export const PRESENTATION_URL = "http://localhost:3000/presentation.pdf";
